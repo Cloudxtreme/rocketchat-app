5ee507d3d2d51fb7e774ec33bd8cfd14c8e4c941
Johannes Zellner <johannes@nebulon.de>
2015-10-25 12:15:23 +0100

    Fixup the ldap settings

--- start.sh
+++ start.sh
@@ -8,10 +8,10 @@ mkdir -p /app/data/www/
 
 mongo_cli="mongo ${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE} -u ${MONGODB_USERNAME} -p ${MONGODB_PASSWORD}"
 
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Enable\" }, { $set: { value: true }}, { upsert: true })"
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Url\" }, { $set: { value: \"ldap://${LDAP_SERVER}\" }}, { upsert: true })"
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Port\" }, { $set: { value: \"${LDAP_PORT}\" }}, { upsert: true })"
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_DN\" }, { $set: { value: \"${LDAP_USERS_BASE_DN}\" }}, { upsert: true })"
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Bind_Search\" }, { $set: { value: '{\"filter\": \"(&(objectCategory=person)(|(username=#{username})(mail=#{username})))\"}' }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Enable\" }, { \$set: { value: true }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Url\" }, { \$set: { value: \"ldap://${LDAP_SERVER}\" }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Port\" }, { \$set: { value: \"${LDAP_PORT}\" }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_DN\" }, { \$set: { value: \"${LDAP_USERS_BASE_DN}\" }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Bind_Search\" }, { \$set: { value: '{\"filter\": \"(&(objectCategory=person)(|(username=#{username})(mail=#{username})))\"}' }}, { upsert: true })"
 
-node /app/code/bundle/main.js || sleep 10000
+node /app/code/bundle/main.js
