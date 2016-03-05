48d22418b48dc5269e0212f08a89a5b8f342c6d5
girish@cloudron.io <girish@cloudron.io>
2016-03-04 23:36:43 -0800

    ldap settings have changed

--- start.sh
+++ start.sh
@@ -8,10 +8,19 @@ mongo_cli="mongo ${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE} -u ${MONGOD
 
 # LDAP
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Enable\" }, { \$set: { value: true }}, { upsert: true })"
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Url\" }, { \$set: { value: \"ldap://${LDAP_SERVER}\" }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Host\" }, { \$set: { value: \"${LDAP_SERVER}\" }}, { upsert: true })"
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Port\" }, { \$set: { value: \"${LDAP_PORT}\" }}, { upsert: true })"
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_DN\" }, { \$set: { value: \"${LDAP_USERS_BASE_DN}\" }}, { upsert: true })"
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Bind_Search\" }, { \$set: { value: '{\"filter\": \"(&(objectCategory=person)(|(username=#{username})(mail=#{username})))\"}' }}, { upsert: true })"
+
+# LDAP Bind
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Use_Custom_Domain_Search\" }, { \$set: { value: true }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Custom_Domain_Search\" }, { \$set: { value: '{\"filter\": \"(&(objectCategory=person)(|(username=#{username})(mail=#{username})))\", \"userDN\": \"${LDAP_BIND_DN}\", \"password\": \"${LDAP_BIND_PASSWORD}\" }' }}, { upsert: true })"
+
+# LDAP User
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Domain_Base\" }, { \$set: { value: \"${LDAP_USERS_BASE_DN}\" }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Username_Field\" }, { \$set: { value: \"username\" }}, { upsert: true })"
+${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Unique_Identifier_Field\" }, { \$set: { value: \"uid\" }}, { upsert: true })"
+
+# LDAP Sync on login
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Sync_User_Data\" }, { \$set: { value: true }}, { upsert: true })"
 # ldap -> user
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Sync_User_Data_FieldMap\" }, { \$set: { value: '{\"username\":\"name\", \"mail\":\"email\"}' }}, { upsert: true })"
