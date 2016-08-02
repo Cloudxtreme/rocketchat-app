cce09fd6432d8edfe48346becaa33caa40cae99a
Girish Ramakrishnan <girish@cloudron.io>
2016-08-02 12:50:01 -0700

    Only enable LDAP and disable Registration on first run
    This allows users to modify them (and we won't overwrite them on updates)

--- start.sh
+++ start.sh
@@ -11,9 +11,18 @@ mkdir -p /run/rocket.chat/babel-cache
 # Note : we do not use env vars because setting env var does not update existing value
 mongo_cli="mongo ${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE} -u ${MONGODB_USERNAME} -p ${MONGODB_PASSWORD}"
 
+first_run=false
+if [[ $(${mongo_cli} --quiet  --eval "db.rocketchat_settings.findOne({ _id: \"LDAP_Enable\" })") == "null" ]]; then
+    echo "This is the first run"
+    first_run=true
+fi
+
 # See https://github.com/RocketChat/Rocket.Chat/blob/master/packages/rocketchat-ldap/server/settings.js
 # LDAP
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Enable\" }, { \$set: { value: true }}, { upsert: true })"
+if [[ "${first_run}" == "true" ]]; then
+    echo "Enabling LDAP on first run"
+    ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Enable\" }, { \$set: { value: true }}, { upsert: true })"
+fi
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Host\" }, { \$set: { value: \"${LDAP_SERVER}\" }}, { upsert: true })"
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Port\" }, { \$set: { value: \"${LDAP_PORT}\" }}, { upsert: true })"
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Encryption\" }, { \$set: { value: \"plain\" }}, { upsert: true })"
@@ -36,7 +45,10 @@ ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"LDAP_Sync_User_Data_
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"Site_Url\" }, { \$set: { value: \"${APP_ORIGIN}\" }}, { upsert: true })"
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"Accounts_AllowUsernameChange\" }, { \$set: { value: false }}, { upsert: true })"
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"Accounts_AllowPasswordChange\" }, { \$set: { value: false }}, { upsert: true })"
-${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"Accounts_RegistrationForm\" }, { \$set: { value: \"Disabled\" }}, { upsert: true })"
+if [[ "${first_run}" == "true" ]]; then
+    echo "Disabling registration on first run"
+    ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"Accounts_RegistrationForm\" }, { \$set: { value: \"Disabled\" }}, { upsert: true })"
+fi
 
 # Email
 ${mongo_cli} --eval "db.rocketchat_settings.update({ _id: \"SMTP_Host\" }, { \$set: { value: \"${MAIL_SMTP_SERVER}\" }}, { upsert: true })"
